02ae7a914f78264b4f660dc1105f096a60f68464
yin-lo <yinlo@hotmail.fr>
2024-03-25 11:41:59 +0100

    stape 4 - Accueil - dynamiser la page d'accueil

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,18 +1,20 @@
 const dataMapper = require('../dataMapper');
 
 const mainController = {
+	// méthode pour la page d'accueil
+	async homePage(req, res) {
+		try {
+			const figurines = await dataMapper.getAllFigurines();
+      res.render('accueil', { figurines });
+		} catch (error) {
+			res.status(500).send('Erreur serveur');
+		}
+	},
 
-  // méthode pour la page d'accueil
-  homePage: (req, res) => {
-   res.render('accueil')
-  },
-
-  // méthode pour la page article
-  articlePage: (req, res) => {
-    res.render('article')
-  }
-
+	// méthode pour la page article
+	articlePage: (req, res) => {
+		res.render('article');
+	},
 };
-
 
 module.exports = mainController;
